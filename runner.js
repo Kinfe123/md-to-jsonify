eac138792f4f44ad933e93efeea4a708a2c70646
Kinfe123 <kinfishtech@gmail.com>
2024-01-29 21:29:24 +0300

    optimistic git api key

--- runner.js
+++ runner.js
@@ -1,9 +1,9 @@
 import { extract ,extractFromLink  } from "./script.js";
 
-const data = await extractFromLink()
+const url =
+  "https://api.github.com/repos/Kinfe123/md-to-json-based-api/git/blobs/9ce4bf952d956457f11c5937ce189aff6270f63c";
+const data = await extractFromLink(url)
 
-// const url =
-//   "https://api.github.com/repos/Kinfe123/md-to-json-based-api/git/blobs/9ce4bf952d956457f11c5937ce189aff6270f63c";
 
 
 // const res = await fetch(url)
--- script.js
+++ script.js
@@ -66,10 +66,9 @@ function processIt(rawData, res) {
   return result;
 }
 
-export const extractFromLink = async () => {
-  const url =
-    "https://api.github.com/repos/Kinfe123/md-to-json-based-api/git/blobs/9ce4bf952d956457f11c5937ce189aff6270f63c";
-  const res = await fetch(url);
+export const extractFromLink = async (link) => {
+  
+  const res = await fetch(link);
   const response = await res.json();
   const content = await response.content;
   const binaryString = await atob(content);
