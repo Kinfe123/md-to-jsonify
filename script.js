dad9a35968849b6187186f922e0dafa4211ac49a
Kinfe123 <kinfishtech@gmail.com>
2024-01-29 15:10:35 +0300

    external file read

--- script.js
+++ script.js
@@ -1,121 +1,76 @@
-const tableMarkdown = `
+const fs = require("fs");
 
+var tableMarkdown = ``;
 
-| Name                                   | Description                                                                                                 | Founded Year |
-| -------------------------------------- | ----------------------------------------------------------------------------------------------------------- | ------------ |
-| [Apideck](https://apideck.com)   | Apideck provides access to a single integration layer with a rapidly growing ecosystem of APIs.                                                                                                                         | 2018         |
-| [Appwrite](https://appwrite.io)   | Appwrite is a secure end-to-end backend server for Web, Mobile, and Flutter developers that is packaged as a set of Docker containers for easy deployment                                                                                                                         | 2020         |
-| [Bannerbear](https://bannerbear.com)   | Bannerbear auto-generates social media visuals, ecommerce banners, and dynamic email images.                | 2019         |
-| [Budibase](https://budibase.com)       | Budibase helps you build internal tools on your own infrastructure in minutes.                              | 2019         |
-| [Cerbos](https://cerbos.dev)         | Cerbos is decoupled, open-source and low-toil application access control. | 2021 |
-| [Chromatic](https://www.chromatic.com) | Chromatic automates gathering UI feedback, visual testing, and documentation, so developers can iterate faster with less manual work.                             | 2017         |
-| [Dub.co](https://dub.co)       | Dub is the link management infrastructure for modern marketing teams.                              | 2023         |
-| [Doppler](https://doppler.com)       | Doppler helps developers manage their API keys across their projects.                              | 2018         |
-| [Feedback Fish](https://feedback.fish/)                  | A simple way to collect feedback & make your customers happy.                                          | 2020         |
-| [Fig](https://fig.io)                  | Fig adds visual apps, shortcuts, and autocomplete to the terminal.                                          | 2020         |
-| [Flightcontrol](https://flightcontrol.dev/)                  | Fullstack Deploy Platform that brings Vercel's world-class DX to your own cloud. Supports both containers & serverless functions, and is optimized for fullstack Next.js & Blitz.js apps                                          | 2021         |
-| [Gitpod](https://gitpod.io)                  | Gitpod automates the provisioning of ready-to-code development environments.                                          | 2019         |
-| [HostedHooks](https://hostedhooks.com)                  | HostedHooks is a webhooks sending as a service platform for developers                                           | 2021         |
-| [Inspect](https://inspect.dev)                  | Inspect helps you debug mobile web apps and websites on iOS devices.                                          | 2020         |
-| [Knock](https://knock.app)                  | Knock is a notifications-as-a-service platform. Send notifications to multiple channels such as in-app, email, push, Slack and more with a single API call.                                          | 2021         |
-| [Linear](https://linear.app)           | Linear helps streamline software projects, sprints, tasks, and bug tracking.                                | 2019         |
-| [Livecycle](https://livecycle.io)           | Livecycle is the inclusive, async collaboration platform for product-centric teams.                                | 2022         |
-| [Mintlify](https://mintlify.com)       | Mintlify helps companies build beautiful documentation that converts users.                                 | 2022         |
-| [Modulz](https://modulz.app)           | Closing the gap between design and dev. Building open source libraries such as Radix and Stitches.          | 2018         |
-| [Nhost](https://nhost.io)              | Nhost is a open source Firebase alternative with GraphQL.                                                   | 2019         |
-| [PlanetScale](https://planetscale.com) | PlanetScale is a serverless database platform that you can start in seconds.                                | 2018         |
-| [Port](https://www.getport.io) | Port is a unified and self service Developer Portal | 2022 |
-| [Prefect](https://prefect.io)          | Prefect is the easiest way to automate data pipelines.                                                      | 2018         |
-| [Railway](https://railway.app)         | Railway helps you develop code in a cloud that feels local.                                                 | 2020         |
-| [Raycast](https://raycast.com)         | Raycast lets you control your tools with a few keystrokes.                                                  | 2020         |
-| [Render](https://render.com)           | Render is a unified cloud to build and run all your apps and websites.                                      | 2018         |
-| [Resend](https://resend.com)           | Resend is an email API for developers.                                                                      | 2023         |
-| [Roboflow](https://roboflow.com)       | Roboflow is a developer tool for building computer vision models faster and more accurately.                | 2019         |
-| [Supabase](https://supabase.io)        | Supabase is the open source Firebase alternative.                                                           | 2020         |
-| [SuperTokens](https://supertokens.io)  | SuperTokens is an open source alternative to Auth0, Okta, Firebase Auth and AWS Cognito.                                                           | 2019         |
-| [Svix](https://www.svix.com)  | Svix makes sending webhooks easy and reliable by offering webhooks sending as a service.                             | 2021         |
-| [Sturdy](https://getsturdy.com) | Real-Time Version Control. Share, and iterate on code together. Like Slack instead of email when coding as a team. | 2021         |
-| [Temporal](https://temporal.io)        | Temporal enables its users to build and operate resilient applications using developer-friendly primitives. | 2019         |
-| [Warrant](https://warrant.dev)         | Warrant provides APIs and infrastructure to help developers implement authorization and access control in their apps. | 2021 |
-| [WorkOS](https://workos.com)           | WorkOS provides APIs to make applications enterprise-ready.                                                 | 2018         |
-
-
-`;
-
-const anotherTableMarkdown = `
-
-| Title                                                                                  | Presenter         | Duration | Date       |
-| -------------------------------------------------------------------------------------- | ----------------- | -------- | ---------- |
-| [The Next Generation of Developer-First Products](https://youtu.be/ja-Pr58FCIQ)                                    | Zeno Rocha     | 43:41    | 5/5/2022   |
-| [Building DevX Teams](https://youtu.be/xX5zeVy8Ta4)                                    | Cirpo Cinelli     | 42:04    | 5/5/2022   |
-| [Things nobody told you about developer experience (DX)](https://youtu.be/gqnWoh8o36M) | DX Doctor         | 17:02    | 11/23/2019 |
-| [The only way how to have happy & productive developers](https://youtu.be/X4NSLo97Az8) | Vratislav Kalenda | 25:18    | 12/8/2018  |
-| [The Seven Deadly Sins of Developer Onboarding](https://youtu.be/JHtMN8TxUfc)          | Cristiano Betta   | 28:03    | 11/13/2017 |
-| [JS UX: Writing code for humans(DX)](https://youtu.be/loj3CLHovt0)                         | Lea Verou         | 1:09:04  | 9/26/2016  |
-| [The Elements of Developer Experience Design](https://youtu.be/lQdeWHPgoMs)       | Stephen Boak      | 5:24     | 8/4/2016   |
-
-
-
-
-`;
-// this will be for mocking the api blob response from readme file
-
-const result = [];
-const trimmed = anotherTableMarkdown.trim();
-const eachRow = trimmed.split("\n");
-
-const headerParser = (row) => {
-  return row
-    .split("|")
-    .map((r) => r.trim())
-    .filter((r) => r !== "");
-};
-
-const headers = headerParser(eachRow[0]);
-
-const theRest = eachRow.slice(2, eachRow.length);
-
-const test = "[Alice's Website](https://example.com/alice)";
-
-const linkFlag = ["https://", "http://"];
-
-// const websiteIdx = headers.indexOf("Website");
-
-for (eachCol of theRest) {
-  let current_parsed = headerParser(eachCol);
- 
-  let counter = 0;
-  let currentObj = {};
-
-  for (eachVal of current_parsed) {
-    let currentKey = headers[counter];
-
-    if (eachVal.includes(linkFlag[0]) || eachVal.includes(linkFlag[1])) {
-      let openSquareIdx = eachVal.indexOf("[");
-      let closeSquareIdx = eachVal.indexOf("]");
-      const add = (eachVal.length - closeSquareIdx)
-      let openIdx = eachVal.slice(closeSquareIdx , eachVal.length).indexOf("(") + closeSquareIdx;
-      let closeIdx = eachVal.slice(closeSquareIdx , eachVal.length).indexOf(")") + closeSquareIdx;
-      // we have to identify the link even if there exit () in link helper
-        
-
-      let parsedLink = eachVal.slice(openIdx + 1, closeIdx);
-      let parsedLinkHelper = eachVal.slice(openSquareIdx + 1, closeSquareIdx);
-      currentObj["link"] = parsedLink;
-      currentObj[currentKey] = parsedLinkHelper;
-      counter+=1;
-      continue;
-    } else {
-      currentObj[currentKey] = eachVal;
+export const extract = (filePath) => {
+  fs.readFile("readme.md", "utf-8", (err, tableMarkdown) => {
+    if (err) {
+      console.log("Error has occured", err);
+      return;
     }
-    counter += 1;
 
+    // this will be for mocking the api blob response from readme file
 
-    
-    
-  }
-  result.push(currentObj);
-}
+    const result = [];
+    const trimmed = tableMarkdown.trim();
+    const eachRow = trimmed.split("\n");
+
+    const headerParser = (row) => {
+      return row
+        .split("|")
+        .map((r) => r.trim())
+        .filter((r) => r !== "");
+    };
+
+    const headers = headerParser(eachRow[0]);
+
+    const theRest = eachRow.slice(2, eachRow.length);
+
+    const test = "[Alice's Website](https://example.com/alice)";
+
+    const linkFlag = ["https://", "http://"];
+
+    // const websiteIdx = headers.indexOf("Website");
+
+    for (eachCol of theRest) {
+      let current_parsed = headerParser(eachCol);
+
+      let counter = 0;
+      let currentObj = {};
+
+      for (eachVal of current_parsed) {
+        let currentKey = headers[counter];
+
+        if (eachVal.includes(linkFlag[0]) || eachVal.includes(linkFlag[1])) {
+          let openSquareIdx = eachVal.indexOf("[");
+          let closeSquareIdx = eachVal.indexOf("]");
+          const add = eachVal.length - closeSquareIdx;
+          let openIdx =
+            eachVal.slice(closeSquareIdx, eachVal.length).indexOf("(") +
+            closeSquareIdx;
+          let closeIdx =
+            eachVal.slice(closeSquareIdx, eachVal.length).indexOf(")") +
+            closeSquareIdx;
+          // we have to identify the link even if there exit () in link helper
+
+          let parsedLink = eachVal.slice(openIdx + 1, closeIdx);
+          let parsedLinkHelper = eachVal.slice(
+            openSquareIdx + 1,
+            closeSquareIdx
+          );
+          currentObj["link"] = parsedLink;
+          currentObj[currentKey] = parsedLinkHelper;
+          counter += 1;
+          continue;
+        } else {
+          currentObj[currentKey] = eachVal;
+        }
+        counter += 1;
+      }
+      result.push(currentObj);
+    }
+  });
+};
 
 console.log("The result is: ", result);
 
