ca6b46e5d400fbddc1e32b5341d5d58a9f077ede
Kinfe123 <kinfishtech@gmail.com>
2024-01-30 13:46:24 +0300

    fix:some minor fix

--- runner.js
+++ runner.js
@@ -1,17 +1,14 @@
-import { extract ,extractFromLink  } from "./script.js";
+import { extract, extractFromLink } from "./script.js";
 
 const url =
   "https://api.github.com/repos/Kinfe123/md-to-json-based-api/git/blobs/9ce4bf952d956457f11c5937ce189aff6270f63c";
-const data = await extractFromLink(url)
-
-
+const data = await extractFromLink(url);
 
 // const res = await fetch(url)
 // const response = await res.json()
 // const content = await response.content
 // const binaryString = await atob(content)
 
+console.log("The data is: ", data);
 
-console.log('The data is: ', data)
-
-  // console.log("THe data is: ", data);
+// console.log("THe data is: ", data);
